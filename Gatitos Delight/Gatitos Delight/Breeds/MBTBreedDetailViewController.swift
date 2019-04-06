5af6dcdaa9d7a7eb6c3ddb300890b44e577cbe81
BENZI Marco <marco.benzi-tobar@inria.fr>
2019-04-07 01:56:49 +0200

    Added functionality to load 9 images

--- Gatitos Delight/Gatitos Delight/Breeds/MBTBreedDetailViewController.swift
+++ Gatitos Delight/Gatitos Delight/Breeds/MBTBreedDetailViewController.swift
@@ -7,10 +7,23 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
+import Alamofire_SwiftyJSON
+import Kingfisher
 
 class MBTBreedDetailViewController: UIViewController {
 
-    @IBOutlet weak var breedNameLabel: UILabel!
+    @IBOutlet weak var catThumbnail1ImageView: UIImageView!
+    @IBOutlet weak var catThumbnail2ImageView: UIImageView!
+    @IBOutlet weak var catThumbnail3ImageView: UIImageView!
+    @IBOutlet weak var catThumbnail4ImageView: UIImageView!
+    @IBOutlet weak var catThumbnail5ImageView: UIImageView!
+    @IBOutlet weak var catThumbnail6ImageView: UIImageView!
+    @IBOutlet weak var catThumbnail7ImageView: UIImageView!
+    @IBOutlet weak var catThumbnail8ImageView: UIImageView!
+    @IBOutlet weak var catThumbnail9ImageView: UIImageView!
+    
     
     var representedBreed : MBTBreedCellModel? {
         didSet{
@@ -31,6 +44,62 @@ class MBTBreedDetailViewController: UIViewController {
         }
         
         navigationItem.title = breed.name
+        getThumbnails(with: breed.id) { imgurls in
+            var thumbnailsImageViews = [
+             self.catThumbnail1ImageView,
+             self.catThumbnail2ImageView,
+             self.catThumbnail3ImageView,
+             self.catThumbnail4ImageView,
+             self.catThumbnail5ImageView,
+             self.catThumbnail6ImageView,
+             self.catThumbnail7ImageView,
+             self.catThumbnail8ImageView,
+             self.catThumbnail9ImageView
+            ]
+            
+            var i = 0
+            for strurl in imgurls {
+                thumbnailsImageViews[i]?.kf.indicatorType = .activity
+                let url = URL(string: strurl)
+                thumbnailsImageViews[i]?.kf.setImage(with: url)
+                i += 1
+            }
+        }
+    }
+    
+    // MARK: - API request
+    func getThumbnails(with breedID:String, completion: (([String])->())? = nil) {
+        let body: Parameters = [
+            "mime_types": "jpg,png",
+            "breed_ids": breedID,
+            "limit": 9,
+            "size": "thumb"
+        ]
+        
+        Alamofire.request("https://api.thecatapi.com/v1/images/search",
+                          parameters:body,
+                          headers: MBTCatAPIHeader.httpHeader).responseSwiftyJSON {
+                            response in
+                            switch response.result {
+                            case .success:
+                                debugPrint(response)
+                                var strUrls : [String] = []
+                                guard let responseValue = response.result.value else {
+                                    debugPrint("Couldn't get a proper API response.")
+                                    return
+                                }
+                                
+                                for (_,img):(String,JSON) in JSON(responseValue) {
+                                    guard let strUrl = img["url"].string else {
+                                        continue
+                                    }
+                                    strUrls.append(strUrl)
+                                }
+                                completion?(strUrls)
+                            case .failure(let error):
+                                debugPrint(error)
+                            }
+        }
     }
     
 
