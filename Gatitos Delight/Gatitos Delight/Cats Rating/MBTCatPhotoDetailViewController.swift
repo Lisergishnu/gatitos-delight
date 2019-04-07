b1ce990112188743f4132c33569c47079343b191
BENZI Marco <marco.benzi-tobar@inria.fr>
2019-04-07 22:25:13 +0200

    Added photo detail VC

---
+++ Gatitos Delight/Gatitos Delight/Cats Rating/MBTCatPhotoDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MBTCatPhotoDetailViewController.swift
+//  Gatitos Delight
+//
+//  Created by Marco Benzi Tobar on 4/7/19.
+//  Copyright © 2019 MBT. All rights reserved.
+//
+
+import UIKit
+
+class MBTCatPhotoDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
